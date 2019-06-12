cbf85a5fd968db2e26e72bab2903060d1abcabb5
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-06-12 11:23:42 -0500

    Left only view related logic in the sessions helper, moved the rememver and forget methods to the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
-require './app/helpers/sessions_helper.rb'
+
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  def remember(user)
+    user.remember
+    cookies.permanent[:user_id] = user.id
+    cookies.permanent[:remember_digest] = user.remember_digest
+  end
 
+  def forget(user)
+    cookies.delete(:user_id)
+    cookies.delete(:remember_digest)
+  end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,4 @@
 module SessionsHelper
-  
-  def remember(user)
-    user.remember
-    cookies.permanent[:user_id] = user.id
-    cookies.permanent[:remember_digest] = user.remember_digest
-  end
-
-  def forget(user)
-    cookies.delete(:user_id)
-    cookies.delete(:remember_digest)
-  end
 
   def current_user
     if session[:user_id] 
